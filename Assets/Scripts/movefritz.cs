2a9112b5abc0835e34ad4917760f19a489907941
Enzo Allemano <enzo.allemano@rpn.ch>
2015-09-13 21:09:54 +0200

    Animation des personnages + déplacement

---
+++ Assets/Scripts/movefritz.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class movefritz : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.W)) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+		} 
+
+		if (Input.GetKey (KeyCode.D)) {
+			
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+			transform.localScale = new Vector3 (5, 5, 5);
+			anim.SetFloat ("speedfritz", 2);
+
+		} else if (Input.GetKey (KeyCode.A)) {
+			
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+			transform.localScale = new Vector3 (-5, 5, 5);
+			anim.SetFloat ("speedfritz", 2);
+		} 
+
+		else 
+		{
+			anim.SetFloat ("speedfritz", 0);
+		}
+
+
+	}
+}
---
+++ Assets/Scripts/moveromain.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveromain : MonoBehaviour {
+
+	public float moveSpeed;
+	public float jumpHeight;
+
+	//Variable servant pour l'ampêchement des sauts infinis
+	public Transform GroundCheck;
+	public float GroundCheckRadius;
+	public LayerMask WhatIsGround;
+	private bool Grounded;
+
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+	
+		anim = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (GetComponent<Rigidbody2D> ().velocity.x, jumpHeight);
+		}
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+			transform.localScale = new Vector3 (4, 4, 4);
+			anim.SetFloat ("speedromain", 2);
+
+		} else if (Input.GetKey (KeyCode.LeftArrow)) {
+			
+			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
+
+			transform.localScale = new Vector3 (-4, 4, 4);
+			anim.SetFloat ("speedromain", 2);
+		} 
+		else 
+		{
+			anim.SetFloat ("speedromain", 0);
+		}
+
+
+	}
+}
