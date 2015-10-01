f954c0fb26b48b05a9cae93c4a681a7e22d1655b
unknown <schulthessm@LMB-113A-09.s2.rpn.ch>
2015-10-01 16:16:28 +0200

    Amélioration du script de mouvement et correction du spawn

---
+++ Assets/Scripts/MoveCharacter.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveCharacter : MonoBehaviour {
+	protected float moveSpeed=15;
+	protected float jumpHeight=20;
+	public float Timer=3;
+	
+	public int doublejump = 2;
+	const int DOUBLEJUMP = 2;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	protected void respawn(){
+		transform.position = new Vector2 (-24, -40);
+	}
+
+	//Vérifie si le personnage rentre en collision avec un objet du groupe Bonus_Speed et si oui, augmente ça vitesse
+	protected void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.CompareTag ("Bonus_Speed")) {
+			other.gameObject.SetActive (false);
+			moveSpeed = 25;
+		}
+		
+		if (other.gameObject.CompareTag ("Jump_Spring")) {
+			GetComponent<Rigidbody2D> ().velocity = new Vector3 (0, 40, 0);
+		}
+	}
+
+	protected void OnCollisionEnter2D(Collision2D coll)
+	{
+		doublejump = DOUBLEJUMP; // remet la valeur doublejump à 2;
+	}
+}
--- Assets/Scripts/movefritz.cs
+++ Assets/Scripts/movefritz.cs
@@ -1,26 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class movefritz : MonoBehaviour {
-
-	private float moveSpeed=15;
-	private float jumpHeight=20;
-	public float Timer=3;
-
-	public int doublejump = 2;
-	const int DOUBLEJUMP = 2;
-
+public class movefritz : MoveCharacter {
 	private Animator anim;
 
 	//Se lance au démarrage du jeu
 	void Start () {
-		transform.position = new Vector2 (-4, -2);
 		anim = GetComponent<Animator> ();
-
+		transform.localScale = new Vector3 (8,8,3);
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		//Temps pour le bonus de movespeed
 		if (moveSpeed!=15) {
 			Timer -= Time.deltaTime;
@@ -57,7 +48,7 @@ public class movefritz : MonoBehaviour {
 			
 			GetComponent<Rigidbody2D> ().velocity = new Vector2 (-moveSpeed, GetComponent<Rigidbody2D> ().velocity.y);
 
-			transform.localScale = new Vector3 (8,8,3);
+			transform.localScale = new Vector3 (-8,8,3);
 			anim.SetFloat ("speedfritz", 2);
 			if (Input.GetKey (KeyCode.S)) {
 				if (Input.GetKeyUp(KeyCode.S)) {
@@ -70,23 +61,6 @@ public class movefritz : MonoBehaviour {
 		else 
 		{
 			anim.SetFloat ("speedfritz", 0);
-		}
-	}
-	
-	void OnCollisionEnter2D(Collision2D coll)
-	{
-		doublejump = DOUBLEJUMP; // remet la valeur doublejump à 2;
-	}
-
-	//Vérifie si le personnage rentre en collision avec un objet du groupe Bonus_Speed et si oui, augmente ça vitesse
-	void OnTriggerEnter2D(Collider2D other) {
-		if (other.gameObject.CompareTag("Bonus_Speed")) {
-			other.gameObject.SetActive(false);
-			moveSpeed=25;
-		}
-
-		if (other.gameObject.CompareTag("Jump_Spring")) {
-			GetComponent<Rigidbody2D>().velocity=new Vector3(0,40,0);
 		}
 	}
 }
