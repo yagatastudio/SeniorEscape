530b4f44b9eac60a53fe75e51cb6e4fcb1295dc6
Schulthess Maxime <SchulthessM@s2.rpn.ch>
2015-10-22 16:18:28 +0200

    Fin + pause

---
+++ Assets/Scripts/Fin.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fin : fin2
+{
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Friz"))
+        {
+            Time.timeScale = 0f;
+            gagnant = 0;
+            Application.LoadLevel(2);
+        }
+        if (other.gameObject.CompareTag("Romain"))
+        {
+            Time.timeScale = 0f;
+            gagnant = 1;
+            Application.LoadLevel(2);
+        }
+    }
+}
