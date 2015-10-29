bfa3c4a23f301a6fa8a4274d24e710869a9c2424
Allemano Enzo <AllemanoEn@s2.rpn.ch>
2015-10-29 16:00:34 +0100

    Améliorer le menu + ajout du fading + wallpaper menu

---
+++ Assets/Scripts/Fade.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts
+{
+    class Fade
+    {
+        internal float BeginFade(int p)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Assets/Scripts/menu.cs
+++ Assets/Scripts/menu.cs
@@ -1,10 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
+using Assets.Scripts;
 
 public class menu : MonoBehaviour {
 
 	public void StartGame()
     {
+        float fadeTime = GameObject.Find("fading").GetComponent<Fading>().BeginFade(1);
+        
         Application.LoadLevel(1);
     }
 
